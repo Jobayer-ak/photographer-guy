8a0f5d930ac2ebda0709790e8a90829269e66e20
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-22 18:14:39 +0600

    navbar and logo added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,11 +1,28 @@
+import logo from "../../images/logo.png";
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h2>This is Header</h2>
-    </div>
+    <>
+      <Navbar variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            {" "}
+            Fotografa
+            <img height="40" src={logo} alt="Photographer Guy" />
+          </Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Services</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+            <Nav.Link href="#pricing">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
