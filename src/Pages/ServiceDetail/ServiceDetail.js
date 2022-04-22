8e0995cb79335123ba4516dc30e46d0d46b70802
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-22 16:54:49 +0600

    most of the components are created

---
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ServiceDetail.css";
+
+const ServiceDetail = () => {
+  return (
+    <div>
+      <h2>This is Service Details</h2>
+    </div>
+  );
+};
+
+export default ServiceDetail;
